e5ea2d1dc8e5a9711b9b1cd3f3bc1bfbd137b490
alvarobarquinc <alvarobarquinc@gmail.com>
2023-11-09 02:08:05 -0300

    Seccion producto terminado en html y css

---
+++ sprint2/sprint2/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//elementos estaticos
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+//levantamos el sv
+app.listen(8000, () => console.log('Servidor corriendo'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+app.get('/producto', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/producto.html'));
+})
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+})
