016703dafa3dd517382e836e3ac17efe77e883c6
gabrielapaz01 <gabiipaz200139@gmail.com>
2023-11-16 17:02:51 -0300

    Corrigiendo la ubicación y detalles de html y css del registro

--- sprint2/sprint2/src/app.js
+++ sprint2/sprint2/src/app.js
@@ -9,12 +9,15 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 //levantamos el sv
 app.listen(8000, () => console.log('Servidor corriendo'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-})
-app.get('/producto', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/producto.html'));
-})
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+//app.get('/', (req, res) => {
+  //  res.sendFile(path.join(__dirname, '/views/home.html'));
+//})
+//app.get('/producto', (req, res) => {
+   // res.sendFile(path.join(__dirname, '/views/producto.html'));
+//})
+//app.get('/carrito', (req, res) => {
+   // res.sendFile(path.join(__dirname, '/views/carrito.html'));
+//})
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/registro.html'));
 })
